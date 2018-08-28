7fb785a34631cac0660c1559e01740c9f3ed30a9
sercankulig <sercan.kulig@krontech.com>
2018-08-28 14:21:24 +0300

    start

---
+++ src/main/java/com/example/SpringBootBatchExample/SpringBootBatchExample/SpringBootBatchExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringBootBatchExample.SpringBootBatchExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchExampleApplication.class, args);
+	}
+}
